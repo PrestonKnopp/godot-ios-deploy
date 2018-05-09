1b5085a2b2e4e5ce5f60d486e61d414c79157832
indicainkwell <indicainkwell@gmail.com>
2018-05-08 22:52:10 -0700

    removed duplicate method in v3 logger

--- addons/com.indicainkwell.iosdeploy/scripts/_v3/logger.gd
+++ addons/com.indicainkwell.iosdeploy/scripts/_v3/logger.gd
@@ -4,15 +4,11 @@ extends '../logger.gd'
 
 func _initialize():
 	._initialize()
-	ProjectSettings.set_settings(stc.LOGGER_DOMAIN, self)
+	ProjectSettings.set_setting(stc.LOGGER_DOMAIN, self)
 
 func cleanup():
 	.cleanup()
 	ProjectSettings.clear(stc.LOGGER_DOMAIN)
-
-static func get_logger():
-	if ProjectSettings.has_setting(stc.LOGGER_DOMAIN):
-		return
 
 static func has_logger():
 	return ProjectSettings.has_setting(stc.LOGGER_DOMAIN)
--- addons/com.indicainkwell.iosdeploy/scripts/logger.gd
+++ addons/com.indicainkwell.iosdeploy/scripts/logger.gd
@@ -16,7 +16,7 @@ class Logfile:
 	var file = null
 	var path = ""
 	var queue_mode = QUEUE_NONE
-	var buffer = StringArray()
+	var buffer = [] # StringArray(), not avail in v3
 	var buffer_idx = 0
 
 	func _init(_path, _queue_mode = QUEUE_NONE):
@@ -238,7 +238,7 @@ const FILE_BUFFER_SIZE = 30
 var default_output_level = INFO
 # TODO: Find (or implement in Godot) a more clever way to achieve that
 var default_output_strategies = [STRATEGY_PRINT, STRATEGY_PRINT, STRATEGY_PRINT, STRATEGY_PRINT, STRATEGY_PRINT]
-var default_logfile_path = "user://%s.log" % Globals.get("application/name")
+var default_logfile_path = "user://com.indicainkwell.ios-deploy.log" # % Globals.get("application/name") # Globals not available in v3
 var default_configfile_path = "user://%s.cfg" % PLUGIN_NAME
 
 # e.g. "[main.INFO]: The young alpaca started growing a goatie."
