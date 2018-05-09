b2a86e560c950d49aebd94eb313adc597548d46f
indicainkwell <indicainkwell@gmail.com>
2018-05-09 01:38:18 -0700

    device_finder don't need to skip line and update json usage

--- addons/com.indicainkwell.iosdeploy/scripts/xcode/finders/device_finder.gd
+++ addons/com.indicainkwell.iosdeploy/scripts/xcode/finders/device_finder.gd
@@ -23,12 +23,12 @@ class Device:
 		Simulator,
 		Mac
 	}
-	
+
 	enum Connection {
 		USB,
 		WIFI
 	}
-	
+
 	var id
 	var name
 	var type_info
@@ -70,35 +70,34 @@ func _init():
 # [....] Found 3345abc45b3cab4c5eb5c4bfb3c5998abc3b320a (P105AP, iPad mini, iphoneos, armv7) a.k.a. 'iPad Name' connected through USB.
 func _ios_deploy_find_devices():
 	var result = []
-	
+
 	var output = _ios_deploy.detect_devices()
-	output.pop_front() # pop [....] Waiting ...
-	
+
 	for line in output:
 		var captures = _regex.search(line)
-		if captures[0].empty():
+		if captures.empty():
 			# Whole pattern didn't match
 			continue
-		
+
 		var device = Device.new()
 		device.id = captures[1]
 		device.type_info = captures[2]
 		device.name = captures[3]
-		
+
 		# extra required capture checks
-		
+
 		# device.type will never be sim or mac
 		# from ios-deploy
 		if device.type_info.find('iPhone') > -1:
 			device.type = Device.Type.iPhone
 		elif device.type_info.find('iPad') > -1:
 			device.type = Device.Type.iPad
-		
+
 		if captures[4].find('USB') == -1:
 			device.connection = Device.Connection.WIFI
-		
+
 		result.append(device)
-	
+
 	return result
 
 
@@ -108,28 +107,28 @@ func _instruments_find_devices():
 	var res = _sh.run(listknowndevices)
 	if res.code != 0:
 		return []
-	
+
 	var devices = []
-	
+
 	# for some reason multiline output is all in first element
 	for line in res.output[0].split('\n', false):
 		# skip sims until add support for x86 project gen
 		if line.find('] (Simulator)') != -1:
 			continue
-		
+
 		var device = Device.new()
 		var end_name_idx = line.rfind('[')
 		device.name = line.substr(0, end_name_idx).strip_edges()
-		
-		
+
+
 		var end_id_idx = line.find(']', end_name_idx)
-		
+
 		# move passed '['
 		end_name_idx += 1
-		
+
 		var id_length = end_id_idx - end_name_idx
 		device.id = line.substr(end_name_idx, id_length)
-		
+
 		device.type = device.Type.Unknown
 		if device.name.findn('macbook') != -1:
 			device.type = device.Type.Mac
@@ -137,7 +136,7 @@ func _instruments_find_devices():
 			device.type = device.Type.iPhone
 		elif device.name.findn('ipad') != -1:
 			device.type = device.Type.iPad
-		
+
 		devices.append(device)
 	return devices
 
