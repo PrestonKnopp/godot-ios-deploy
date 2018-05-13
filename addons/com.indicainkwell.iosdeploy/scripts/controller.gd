06c67237be8de2a4dcb8e29707668119c285e087
indicainkwell <indicainkwell@gmail.com>
2018-05-12 21:50:57 -0700

    controller filters provisioning profiles

--- addons/com.indicainkwell.iosdeploy/scripts/controller.gd
+++ addons/com.indicainkwell.iosdeploy/scripts/controller.gd
@@ -69,6 +69,41 @@ func get_menu():
 	return _settings_menu
 
 
+func filter_provisions(provisions):
+	# Filter out
+	# - expired
+	# - duplicates -- Compare by app_id_name or name? I guess name.
+	var valid_provisions = []
+	var duplicates = {}
+	var today = OS.get_unix_time()
+	for provision in provisions:
+		var expire = OS.get_unix_time_from_datetime(provision.expiration_date)
+		if today > expire: continue
+		if not duplicates.has(provision.name):
+			duplicates[provision.name] = []
+		duplicates[provision.name].append(provision)
+		valid_provisions.append(provision)
+	
+	# for each duplicate:
+	# - check creation_date
+	# - keep latest
+	# - erase oldest
+	for provisions in duplicates.values():
+		var latest = provisions[0]
+		var latest_t = OS.get_unix_time_from_datetime(latest.creation_date)
+		for i in range(1, provisions.size()):
+			var next = provisions[i]
+			var next_t = OS.get_unix_time_from_datetime(next.creation_date)
+			if next_t > latest_t:
+				valid_provisions.erase(latest)
+				latest = next
+				latest_t = next_t
+			else:
+				valid_provisions.erase(next)
+	
+	return valid_provisions
+
+
 # ------------------------------------------------------------------------------
 #                                     Callbacks
 # ------------------------------------------------------------------------------
@@ -108,8 +143,8 @@ func _on_request_fill(menu):
 	menu.fill_devices_group(found_names)
 	found_names.clear()
 
-	for provision in _xcode.finder.find_provisions():
-		found_names.append(provision.app_id_name)
+	for provision in filter_provisions(_xcode.finder.find_provisions()):
+		found_names.append(provision.name)
 	menu.populate_profiles(found_names)
 	found_names.clear()
 
