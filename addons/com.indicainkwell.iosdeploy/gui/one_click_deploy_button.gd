71739f5e0aa4426c2f74ceeeae48388205059350
PrestonKnopp <prestonknopp@gmail.com>
2018-08-27 13:53:20 -0700

    better hover panel handling

--- addons/com.indicainkwell.iosdeploy/gui/one_click_deploy_button.gd
+++ addons/com.indicainkwell.iosdeploy/gui/one_click_deploy_button.gd
@@ -10,7 +10,101 @@ signal devices_list_edited(this)
 
 const stc = preload('../scripts/static.gd')
 
+# ------------------------------------------------------------------------------
+#                                    Mouse Hover
+# ------------------------------------------------------------------------------
 
+# Godotv2 InputEventMouseMotion Constant
+const MOUSE_MOTION = 2
+
+# Create timers in script to save time from having to set timeout in both Godot
+# v2 and v3 editor.
+# Start enter timer when mouse enters self
+var _hover_enter_timer
+# Start exit timer after hover panel is shown and mouse no longer hovers them
+var _hover_exit_timer
+
+
+func _ready():
+	_hover_enter_timer = Timer.new()
+	_hover_enter_timer.set_one_shot(true)
+	_hover_enter_timer.set_wait_time(0.75)
+	_hover_enter_timer.connect('timeout', self, '_on_hover_enter_timer_timeout')
+
+	_hover_exit_timer = Timer.new()
+	_hover_exit_timer.set_one_shot(true)
+	_hover_exit_timer.set_wait_time(0.25)
+	_hover_exit_timer.connect('timeout', self, '_on_hover_exit_timer_timeout')
+
+	add_child(_hover_enter_timer)
+	add_child(_hover_exit_timer)
+
+	set_process_input(false)
+
+
+func _on_mouse_entered():
+	"""
+	Connect to mouse entered to save processing time that would be spent in
+	_input() by kicking off the hover enter timer and process_input() here
+	and turning it off when not hovering anymore.
+	"""
+	if is_processing_input():
+		return
+	_hover_enter_timer.start()
+	set_process_input(true)
+
+
+func _input(event):
+	var event_is_InputEventMouseMotion
+	if stc.get_version().is2():
+		event_is_InputEventMouseMotion = event.type == MOUSE_MOTION
+	else:
+		event_is_InputEventMouseMotion = event.get_class() == 'InputEventMouseMotion'
+	
+	if event_is_InputEventMouseMotion:
+		var event_pos
+		if stc.get_version().is2():
+			event_pos = event.pos
+		else:
+			event_pos = event.position
+
+		# - When mouse is NOT hovering self and hp is hidden stop enter
+		# timer
+		# - When mouse is hovering self or hp stop exit timer
+		# - When mouse is NOT hovering self or hp and hp is visible then
+		# start exit timer
+		var hp = get_node('hover_panel')
+		var hp_hidden = check_is_hidden(hp)
+
+		# grow self rect because tool buttons border is larger then rect
+		# and will highlight when rect does not have point
+		var self_has_point = get_rect().grow(4.0).has_point(event_pos)
+		var hp_has_point
+
+		if hp_hidden:
+			if not self_has_point:
+				_hover_enter_timer.stop()
+				set_process_input(false)
+		else:
+			hp_has_point = hp.get_rect().has_point(event_pos)
+			if self_has_point or hp_has_point:
+				_hover_exit_timer.stop()
+			else:
+				_hover_exit_timer.start()
+
+
+func _on_hover_enter_timer_timeout():
+	emit_signal('presenting_hover_menu', self)
+
+	var hover_panel = get_node('hover_panel')
+	hover_panel.set_as_toplevel(true)
+	_place_panel(hover_panel)
+	hover_panel.show()
+
+
+func _on_hover_exit_timer_timeout():
+	set_process_input(false)
+	get_node('hover_panel').hide()
 
 
 func get_devices_list():
@@ -99,54 +193,6 @@ func _draw():
 
 func _on_build_progress_bar_changed():
 	update()
-
-
-func _on_mouse_enter():
-	if check_is_hidden(get_node('hover_panel')):
-		get_node('hover_timer').start()
-	else:
-		get_node('hover_timer').stop()
-
-
-func _on_mouse_exit():
-	if check_is_hidden(get_node('hover_panel')):
-		get_node('hover_timer').stop()
-	else:
-		get_node('hover_timer').start()
-
-
-func _on_hover_panel_input_event(e):
-	# hack, mouse_exit is emitted when over an active child control
-	# within the bounds of target control
-	#
-	# this will stop hover_timer when any event is received as that means
-	# mouse is hovering panel
-	if stc.get_version().is2():
-		if get_node('hover_timer').is_active():
-			get_node('hover_timer').stop()
-	else:
-		if not get_node('hover_timer').is_stopped():
-			get_node('hover_timer').stop()
-
-
-func _on_hover_panel_mouse_enter():
-	get_node('hover_timer').stop()
-
-
-func _on_hover_panel_mouse_exit():
-	get_node('hover_timer').start()
-
-
-func _on_hover_timer_timeout():
-	if check_is_hidden(get_node('hover_panel')):
-		emit_signal('presenting_hover_menu', self)
-
-		var hover_panel = get_node('hover_panel')
-		hover_panel.set_as_toplevel(true)
-		_place_panel(hover_panel)
-		hover_panel.show()
-	else:
-		get_node('hover_panel').hide()
 
 
 func _on_settings_button_pressed():
